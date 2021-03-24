3ed626a5aea65605f5b1040674f0a72207905f3f
davitbzh <davit@logicalclocks.com>
2021-03-24 21:10:56 +0100

    testing

--- hsfs_transformers/core.py
+++ hsfs_transformers/core.py
@@ -3,10 +3,6 @@ from pyspark.sql.functions import udf
 # Use udf to define a row-at-a-time udf
 @udf('double')
 # Input/output are both a single double value
-# Input/output are both a single double value
-# Input/output are both a single double value
-# Input/output are both a single double value
-
 
 def plus_one(value):
       return value + 1
