c910deb02b29e02188fb9d420d4113690afab801
davitbzh <davit@logicalclocks.com>
2021-04-22 09:02:41 +0200

    pure python code

--- hsfs_transformers/core.py
+++ hsfs_transformers/core.py
@@ -1,10 +1,7 @@
 import numpy as np
 import pandas as pd
-from pyspark.sql.functions import udf
 
-# Use udf to define a row-at-a-time udf
-@udf('double')
-# Input/output are both a single double value
+
 def plus_one(value):
       return value + 1
 
