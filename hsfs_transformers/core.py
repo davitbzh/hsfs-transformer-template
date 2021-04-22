9e5000dc2a87e770912289e65f9d786e97690fdb
davitbzh <davit@logicalclocks.com>
2021-04-22 02:42:55 +0200

    --

--- hsfs_transformers/core.py
+++ hsfs_transformers/core.py
@@ -2,22 +2,9 @@ import numpy as np
 import pandas as pd
 from pyspark.sql.functions import udf
 
-from hsfs import util, engine, training_dataset_feature
-from hsfs.statistics_config import StatisticsConfig
-from hsfs.storage_connector import StorageConnector
-from hsfs.core import (
-      training_dataset_api,
-      training_dataset_engine,
-      tfdata_engine,
-      statistics_engine,
-)
-from hsfs.constructor import query
-
-from hsfs_transformers.pandas_udfs import pandas_plus_one
 # Use udf to define a row-at-a-time udf
 @udf('double')
 # Input/output are both a single double value
-
 def plus_one(value):
       return value + 1
 
