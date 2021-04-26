827250c191748b3591d0abc700b5a08271c2ca3b
davitbzh <davit@logicalclocks.com>
2021-04-26 20:59:13 +0200

    get_timestamp_from_date_string

--- hsfs_transformers/core.py
+++ hsfs_transformers/core.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
-
+from datetime import datetime
 
 def plus_one(value):
       return value + 1
+
+def get_timestamp_from_date_string(input_date):
+      date_format = "%Y%m%d%H%M%S"
+      return int(float(datetime.strptime(input_date, date_format).timestamp()) * 1000)
+
 
