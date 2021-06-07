c84593fd686fecde40c59a3abd38259348b32b89
davitbzh <davit@logicalclocks.com>
2021-06-07 20:44:45 +0200

    normilisers

--- hsfs_transformers/normalizers.py
+++ hsfs_transformers/normalizers.py
@@ -1,7 +1,6 @@
 def min_max_normalizer(x):
     if x is None:
         x = 0
-    min = 0
+    min = -1
     max = 2000
-    range = max - min
-    return [(a - min) / range for a in x]
+    return (x - min) / max - min
